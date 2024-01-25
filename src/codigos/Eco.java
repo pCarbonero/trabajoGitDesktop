0ddcda9850574f96c0f319964d70634997dcb864
jlmejorada <joseluis.mejorada@iesnervion.es>
2024-01-25 14:21:37 +0100

    [New] Clase Eco
    Creación de la clase Eco

---
+++ src/codigos/Eco.java
@@ -0,0 +1,10 @@
+package codigos;
+
+public class Eco {
+	// creamos la funcion eco que recibe un int
+	public static void eco(int num) {
+		for (int i = 1; i <= num; i++) {
+			System.out.println("Eco");
+		}
+	}
+}
