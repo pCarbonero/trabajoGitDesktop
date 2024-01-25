4d277214a4fcb963aea94a5d3dc4fb203c5925ed
pCarbonero <pablo.carbonero@iesnervion.es>
2024-01-25 14:20:31 +0100

    [new] codigo main
    terminado el código de la clase main

--- src/codigos/Main.java
+++ src/codigos/Main.java
@@ -1,9 +1,25 @@
 package codigos;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// creamos la variable para guardar un número
+				int num;
+				// creamos el escaner
+				Scanner sc = new Scanner(System.in);
+				// preguntamos por el número
+				System.out.println("simnim");
+				// guardamos el número introducido n la variable num
+				num = sc.nextInt();
+
+				// llamamos la funcion eco de la clase Eco y le pasamos el valor introducido a
+				// su variable int
+				Eco.eco(num);
+
+				// cerramos el escaner
+				sc.close();
 
 	}
 
